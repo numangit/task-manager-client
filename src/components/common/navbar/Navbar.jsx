54607641a8c52be831652c1e14799b2e91812b5a
numangit <abdulnumanjob@gmail.com>
2023-06-05 21:06:56 +0600

    navbar made conditional

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -1,24 +1,37 @@
-// import logo from '/src/assets/logo';
-
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../context/AuthProvider";
+import { useContext } from "react";
+import logo from '../../../assets/logo.png';
 
 const Navbar = () => {
+  const { user, logOut } = useContext(AuthContext);
+
+  //function to handle logout
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {
+        localStorage.removeItem('woodpecker-token');
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <Link to="/" className="flex items-center">
-          {/* <img src={logo} className="h-6 mr-3 sm:h-9" alt="do.it Logo" /> */}
+          <img src={logo} className="h-6 mr-3 sm:h-9" alt="do.it Logo" />
           <span className="self-center text-xl whitespace-nowrap dark:text-white text-blue-800 font-logo font-extrabold">Task Manager</span>
         </Link>
         <div className="flex md:order-2">
-
-          <Link to="/">
-            <button type="button" className="text-white bg-orange-500 hover:bg-orange-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700">Log Out</button>
-          </Link>
-          <Link to="/login">
-            <button type="button" className="text-white  bg-orange-500 hover:bg-orange-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700">Log In</button>
-          </Link>
-
+          {
+            user?.uid
+              ? <Link to="/">
+                <button onClick={handleLogOut} type="button" className="text-white bg-orange-500 hover:bg-orange-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700">Sign Out</button>
+              </Link>
+              : <Link to="/login">
+                <button type="button" className="text-white  bg-orange-500 hover:bg-orange-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700">Sign In</button>
+              </Link>
+          }
 
           <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
@@ -27,20 +40,23 @@ const Navbar = () => {
         </div>
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-
-            <li>
-              <Link to="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base">Home</Link>
-            </li>
-            <li>
-              <Link to="/add-task" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base">Add Task</Link>
-            </li>
-            <li>
-              <Link to="/my-tasks" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base">My Tasks</Link>
-            </li>
-            <li>
-              <Link to="/completed" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base">Completed</Link>
-            </li>
-
+            {
+              user?.uid &&
+              <>
+                <li>
+                  <Link to="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base">Home</Link>
+                </li>
+                <li>
+                  <Link to="/add-task" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base">Add Task</Link>
+                </li>
+                <li>
+                  <Link to="/my-tasks" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base">My Tasks</Link>
+                </li>
+                <li>
+                  <Link to="/completed" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base">Completed</Link>
+                </li>
+              </>
+            }
           </ul>
         </div>
       </div>
