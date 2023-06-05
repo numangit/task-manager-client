3f42acf662566a62b30ea1cb3613c594dcd0b9b5
numangit <abdulnumanjob@gmail.com>
2023-06-05 19:56:03 +0600

    routes added

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from "../pages/home/Home";
+import MyTask from "../pages/myTask/MyTask";
+import AddTask from "../pages/AddTask/AddTask";
+import Completed from "../pages/completed/Completed";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+import TaskDetails from "../pages/taskDetails/taskDetails";
+import Update from "../pages/update/Update";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +17,36 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home></Home>
+      }, ,
+      {
+        path: '/addTask',
+        element: <AddTask></AddTask>
+      },
+      {
+        path: '/myTasks',
+        element: <MyTask></MyTask>
+      },
+      {
+        path: '/completed',
+        element: <Completed></Completed>
+      },
+      {
+        path: '/signIn',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+      {
+        path: '/taskDetails/:id',
+        element: <TaskDetails></TaskDetails>,
+        loader: ({ params }) => fetch(`http://localhost:5000/myTask/${params.id}`)
+      },
+      {
+        path: '/myTasks/edit/:id',
+        element: <Update></Update>,
+        loader: ({ params }) => fetch(`http://localhost:5000/myTask/${params.id}`)
       }
     ]
   }
